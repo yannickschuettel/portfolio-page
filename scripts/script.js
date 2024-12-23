157f676b25b947d59a3c49bfe955cd4ce43e4044
Schuettu <anivvia.anivvia@gmail.com>
2024-12-23 18:56:19 +0100

    hinzufügen einer interaktiven Karte

--- scripts/script.js
+++ scripts/script.js
@@ -3,4 +3,17 @@ console.log("Portfolio-Seite erfolgreich geladen!");
 // Einfaches Beispiel: Begrüßung in der Konsole
 document.addEventListener("DOMContentLoaded", () => {
   alert("Willkommen auf meiner Portfolioseite!");
+
+  // Leaflet-Karte erstellen
+  const map = L.map("mapid").setView([46.9481, 7.4474], 13); // Bern Koordinaten [Latitude, Longitude]
+
+  // OpenStreetMap Tile Layer hinzufügen
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  // Marker in Bern setzen
+  const marker = L.marker([46.9481, 7.4474]).addTo(map);
+  marker.bindPopup("<b>Bern!</b><br>Hauptstadt der Schweiz.").openPopup();
 });
