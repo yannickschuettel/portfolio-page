4ffc520b2643f187285956514780fc722cdfcfc1
Schuettu <anivvia.anivvia@gmail.com>
2024-12-23 15:14:21 +0100

    Anfangsstand der Portofolie Seite erstellt mit grober Struktur

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+console.log("Portfolio-Seite erfolgreich geladen!");
+
+// Einfaches Beispiel: Begrüßung in der Konsole
+document.addEventListener("DOMContentLoaded", () => {
+  alert("Willkommen auf meiner Portfolioseite!");
+});
